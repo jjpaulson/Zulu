c11cb7c49aa7cf2ae1408bf9a027716c2a0bd72f
Jonathan Paulson <jjpaulson@jonathans-macbook-pro.local>
2018-03-24 13:52:09 -0500

    Implemented camera access.

--- Zulu/ZephyrViewController.swift
+++ Zulu/ZephyrViewController.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ZephyrViewController: UIViewController {
+class ZephyrViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     var store = DataStore.sharedInstance
     
+    var captureSession : AVCaptureSession?
+    var videoPreviewLayer : AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView : UIView?
     
+    @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var namingTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         loadData()
+        
         // Do any additional setup after loading the view.
+        
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera device")
+            return
+        }
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Set the input device on the capture session.
+            captureSession?.addInput(input)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            // Start video capture.
+            captureSession?.startRunning()
+            
+            // Move the message label and top bar to the front
+            view.bringSubview(toFront: messageLabel)
+        } catch {
+            // If any error occurs, simply print it out and don't continue any more.
+            print(error)
+            return
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
