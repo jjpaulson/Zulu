a2f747d918826126f93bf71b78e9745cc7114229
AGeubelleGit <amjg2102@gmail.com>
2018-04-15 21:39:41 -0500

    store statistics

--- Zulu/AdminScreenViewController.swift
+++ Zulu/AdminScreenViewController.swift
@@ -12,6 +12,7 @@ class AdminScreenViewController: UIViewController {
 
     @IBOutlet weak var InventoryButton: UIButton!
     @IBOutlet weak var CheckReceiptsButton: UIButton!
+    @IBOutlet weak var storeDataButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class AdminScreenViewController: UIViewController {
         
         CheckReceiptsButton.layer.cornerRadius = 10
         CheckReceiptsButton.clipsToBounds = true
+        
+        storeDataButton.layer.cornerRadius = 10
+        storeDataButton.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
